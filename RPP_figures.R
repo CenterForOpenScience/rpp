a109387d3677a0ca6a46fb3aab4ab33405226e39
FredHasselman <me@fredhasselman.com>
2015-09-04 20:33:08 +0200

    Test Github auto .zip

--- RPP_figures.R
+++ RPP_figures.R
@@ -26,7 +26,7 @@ source_url('https://raw.githubusercontent.com/FredHasselman/toolboxR/master/C-3P
 in.IT(c('ggplot2','RColorBrewer','lattice','gridExtra','plyr','dplyr','httr'))
 
 # Read the data from the OSF storage
-# Note: get.OSFfile() returns a list with the Excel data (df) and information (info) containing the URL download timestamp and original column and rownames (these names will be changed if dfCln=TRUE).  
+# Note: get.OSFfile() returns a list with the .csv data (df) and information (info) containing the URL download timestamp and original column and rownames (these names will be changed if dfCln=TRUE).  
 RPPdata <- get.OSFfile(code='https://osf.io/fgjvw/',dfCln=T)$df
 
 ## If you dowloaded the csv file to your harddrive use this code:
